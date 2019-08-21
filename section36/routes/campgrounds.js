00567ffddd45b5c366bf9e323fc241e81936d1ee
Rafael Marques <rafa.marques.learn@gmail.com>
2019-08-21 15:43:16 -0300

    Updated code for section 36

--- section36/routes/campgrounds.js
+++ section36/routes/campgrounds.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 
-router.get("/", isLoggedIn, function(req, res) {
-  Campground.find({}, function(err, allCampgrounds) {
+router.get("/", function (req, res) {
+  Campground.find({}, function (err, allCampgrounds) {
     if (err) {
       console.log(err);
     } else {
@@ -12,20 +12,25 @@ router.get("/", isLoggedIn, function(req, res) {
   });
 });
 
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
   res.render("campgrounds/new");
 });
 
-router.post("/", isLoggedIn, function(req, res) {
+router.post("/", isLoggedIn, function (req, res) {
   var name = req.body.name;
   var imageUrl = req.body.imageUrl;
   var description = req.body.description;
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
   var newCampground = {
     name: name,
     imageUrl: imageUrl,
-    description: description
+    description: description,
+    author: author
   };
-  Campground.create(newCampground, function(err, addCampground) {
+  Campground.create(newCampground, function (err, addCampground) {
     if (err) {
       console.log(err);
     } else {
@@ -34,10 +39,10 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
-router.get("/:id", isLoggedIn, function(req, res) {
+router.get("/:id", function (req, res) {
   Campground.findById(req.params.id)
     .populate("comments")
-    .exec(function(err, foundCampground) {
+    .exec(function (err, foundCampground) {
       if (err) {
         console.log(err);
       } else {
