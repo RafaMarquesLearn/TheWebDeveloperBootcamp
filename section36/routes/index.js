91aa2187ba08eb6250f6a52e33e2acd2cd748047
Rafael Marques <rafa.marques.learn@gmail.com>
2019-08-22 22:33:47 -0300

    Added code for section 37

--- section36/routes/index.js
+++ section36/routes/index.js
@@ -3,28 +3,28 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   res.render("landing");
 });
 
-router.get("/register", function (req, res) {
+router.get("/register", function(req, res) {
   res.render("register");
 });
 
-router.post("/register", function (req, res) {
+router.post("/register", function(req, res) {
   var newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, function (err, user) {
+  User.register(newUser, req.body.password, function(err, user) {
     if (err) {
       console.log(err);
       return res.render("register");
     }
-    passport.authenticate("local")(req, res, function () {
+    passport.authenticate("local")(req, res, function() {
       res.redirect("/campgrounds");
     });
   });
 });
 
-router.get("/login", function (req, res) {
+router.get("/login", function(req, res) {
   res.render("login");
 });
 
@@ -36,7 +36,7 @@ router.post(
   })
 );
 
-app.get("/logout", function (req, res) {
+router.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
